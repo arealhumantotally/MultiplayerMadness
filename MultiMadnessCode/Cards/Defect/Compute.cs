f0968c48072d8ba3d765ee2003ca14e13faf9d57
Me <No>
2026-04-16 15:27:30 -0400

    Added Distributed Computing

---
+++ MultiMadnessCode/Cards/Defect/Compute.cs
@@ -0,0 +1,33 @@
+using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models.CardPools;
+using MultiMadness.MultiMadnessCode.Cards;
+
+namespace MultiMadness.MultiMadnessCode.Cards.Defect;
+
+[Pool(typeof(TokenCardPool))]
+public class Compute() : MultiMadnessCard(0,
+    CardType.Skill, CardRarity.Token,
+    TargetType.AnyAlly)
+{
+    protected override bool HasEnergyCostX => true;
+    protected override IEnumerable<DynamicVar> CanonicalVars => [];
+
+    protected override async Task OnPlay(
+        PlayerChoiceContext choiceContext,
+        CardPlay play)
+    {
+        int paid = this.ResolveEnergyXValue();
+        PlayerCmd.GainEnergy(paid, play.Target.Player);
+    }
+
+    public override IEnumerable<CardKeyword> CanonicalKeywords => [CardKeyword.Exhaust, CardKeyword.Retain];
+
+    protected override void OnUpgrade()
+    {
+        
+    }
+}
---
+++ MultiMadnessCode/Cards/Defect/DistributedComputing.cs
@@ -0,0 +1,41 @@
+using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Players;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.CardPools;
+using MegaCrit.Sts2.Core.Models.Cards;
+using MultiMadness.MultiMadnessCode.Cards;
+
+namespace MultiMadness.MultiMadnessCode.Cards.Defect;
+
+[Pool(typeof(DefectCardPool))]
+public class DistributedComputing() : MultiMadnessCard(1,
+    CardType.Power, CardRarity.Uncommon,
+    TargetType.Self)
+{
+    protected override IEnumerable<DynamicVar> CanonicalVars => [];
+
+    protected override IEnumerable<IHoverTip> ExtraHoverTips => [HoverTipFactory.FromCard<Compute>()];
+
+    protected override async Task OnPlay(
+        PlayerChoiceContext choiceContext,
+        CardPlay play)
+    {
+        foreach (Player i in this.CombatState.Players)
+        {
+            if (!i.Creature.IsAlive) continue;
+            if (i == this.Owner) continue;
+            await CardPileCmd.AddGeneratedCardToCombat((CardModel) this.CombatState.CreateCard<Compute>(i), PileType.Hand, true);
+        }
+        
+    }
+
+    protected override void OnUpgrade()
+    {
+        this.EnergyCost.UpgradeBy(1);
+    }
+}
