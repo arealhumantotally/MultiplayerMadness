250ea45de022ac1d52e1ec6a762e31cd73bac297
Me <No>
2026-04-28 03:01:05 -0400

    Added file

---
+++ MultiMadnessCode/Cards/Silent/Pass.cs
@@ -0,0 +1,25 @@
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MultiMadness.MultiMadnessCode.Cards;
+
+namespace MultiMadness.MultiMadnessCode.Cards.Silent;
+
+public class Pass() : MultiMadnessCard(int,
+    CardType.Attack, CardRarity.Basic,
+    TargetType.Self)
+{
+    protected override IEnumerable<DynamicVar> CanonicalVars => [];
+
+    protected override async Task OnPlay(
+        PlayerChoiceContext choiceContext,
+        CardPlay play)
+    {
+        
+    }
+
+    protected override void OnUpgrade()
+    {
+
+    }
+}
